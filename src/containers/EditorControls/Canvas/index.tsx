64675dda7fb0a8f26fb025d64317a233b702aa0b
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2025-04-13 20:49:22 +0200

    Install Phosphor and replace inline svg icons with imports

--- src/containers/EditorControls/Canvas/index.tsx
+++ src/containers/EditorControls/Canvas/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import styles from "../EditorControls.module.scss";
 import { useCanvas } from "@/providers/CanvasProvider";
 import clsx from "clsx";
+import { Cube } from "@phosphor-icons/react";
 
 export const Canvas: React.FC = () => {
   const { toggleFrame, showFrame } = useCanvas();
@@ -16,16 +17,7 @@ export const Canvas: React.FC = () => {
         onClick={toggleFrame}
       >
         <span className="sr-only">Toggle canvas outline</span>
-        <svg
-          className="icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 256 256"
-          aria-hidden="true"
-        >
-          <path d="M225.6,62.64l-88-48.17a19.91,19.91,0,0,0-19.2,0l-88,48.17A20,20,0,0,0,20,80.19v95.62a20,20,0,0,0,10.4,17.55l88,48.17a19.89,19.89,0,0,0,19.2,0l88-48.17A20,20,0,0,0,236,175.81V80.19A20,20,0,0,0,225.6,62.64ZM128,36.57,200,76,128,115.4,56,76ZM44,96.79l72,39.4v76.67L44,173.44Zm96,116.07V136.19l72-39.4v76.65Z" />
-        </svg>
+        <Cube weight="bold" className="icon" />
       </button>
     </div>
   );
--- src/containers/EditorControls/Zoom/index.tsx
+++ src/containers/EditorControls/Zoom/index.tsx
@@ -3,6 +3,7 @@
 import React, { useContext } from "react";
 import styles from "../EditorControls.module.scss";
 import { ZoomContext, MIN_ZOOM, MAX_ZOOM } from "@/providers/ZoomProvider";
+import { ArrowClockwise, Minus, Plus } from "@phosphor-icons/react";
 
 export const Zoom: React.FC = () => {
   const { zoom, zoomIn, zoomOut, resetZoom, isDefaultZoom } = useContext(ZoomContext);
@@ -16,19 +17,7 @@ export const Zoom: React.FC = () => {
           disabled={zoom >= MAX_ZOOM}
         >
           <span className="sr-only">Zoom in</span>
-          <svg
-            className="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 256 256"
-            aria-hidden="true"
-          >
-            <path
-              xmlns="http://www.w3.org/2000/svg"
-              d="M228,128a12,12,0,0,1-12,12H140v76a12,12,0,0,1-24,0V140H40a12,12,0,0,1,0-24h76V40a12,12,0,0,1,24,0v76h76A12,12,0,0,1,228,128Z"
-            />
-          </svg>
+          <Plus weight="bold" className="icon" aria-hidden="true" />
         </button>
         <button 
           className={styles.button}
@@ -37,19 +26,7 @@ export const Zoom: React.FC = () => {
           disabled={zoom <= MIN_ZOOM}
         >
           <span className="sr-only">Zoom out</span>
-          <svg
-            className="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 256 256"
-            aria-hidden="true"
-          >
-            <path
-              xmlns="http://www.w3.org/2000/svg"
-              d="M228,128a12,12,0,0,1-12,12H40a12,12,0,0,1,0-24H216A12,12,0,0,1,228,128Z"
-            />
-          </svg>
+          <Minus weight="bold" className="icon" aria-hidden="true" />
         </button>
         <button 
           className={styles.button}
@@ -58,16 +35,7 @@ export const Zoom: React.FC = () => {
           disabled={isDefaultZoom}
         >
           <span className="sr-only">Reset zoom</span>
-          <svg
-            className="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 256 256"
-            aria-hidden="true"
-          >
-            <path d="M244,56v48a12,12,0,0,1-12,12H184a12,12,0,1,1,0-24H201.1l-19-17.38c-.13-.12-.26-.24-.38-.37A76,76,0,1,0,127,204h1a75.53,75.53,0,0,0,52.15-20.72,12,12,0,0,1,16.49,17.45A99.45,99.45,0,0,1,128,228h-1.37A100,100,0,1,1,198.51,57.06L220,76.72V56a12,12,0,0,1,24,0Z" />
-          </svg>
+          <ArrowClockwise weight="bold" className="icon" aria-hidden="true" />
         </button>
       </div>
   );
